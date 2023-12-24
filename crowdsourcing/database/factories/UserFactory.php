366f949bbd801c4feb2947b9f419a097b7b0fd83
markoi95 <marko.ivanovic95@outlook.com>
2023-12-24 16:31:39 +0100

    User mode factory seeder

--- crowdsourcing/database/factories/UserFactory.php
+++ crowdsourcing/database/factories/UserFactory.php
@@ -23,11 +23,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $types = ['admin', 'mod', 'user'];
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'type' => $this->faker->randomElement($types),
             'remember_token' => Str::random(10),
         ];
     }
