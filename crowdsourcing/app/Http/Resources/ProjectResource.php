a8fb86d097c77fe23c73a9f8f9b87858c8f619b2
markoi95 <marko.ivanovic95@outlook.com>
2023-12-27 23:18:44 +0100

    Project controller, collection and resource working

---
+++ crowdsourcing/app/Http/Resources/ProjectResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public static $wrap = 'project';
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->resource->name,
+            'type' => $this->resource->type,
+            'location' => $this->resource->location,
+            'project description' => $this->resource->description,
+            'user' => new UserResource($this->resource->user),
+        ];
+    }
+}
