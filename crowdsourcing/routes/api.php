1b1b3e6b3cdce476bc22e7de7aab96dea555a732
markoi95 <marko.ivanovic95@outlook.com>
2023-12-27 23:19:30 +0100

    api routes update

--- crowdsourcing/routes/api.php
+++ crowdsourcing/routes/api.php
@@ -1,18 +1,22 @@
 <?php
 
+use App\Http\Controllers\DonationController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserProjectController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+/* Route::get('/projects', [ProjectController::class,'index']);
+Route::get('/projects/{id}', [ProjectController::class,'show']); */
+
+/* Route::get('/users/{user_id}/projects', [UserProjectController::class,'index']); */
+
+Route::resource('projects', ProjectController::class);
+Route::resource('users.projects', UserProjectController::class)->only(['index']);
+Route::resource('users', UserController::class);
+Route::resource('donations', DonationController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
