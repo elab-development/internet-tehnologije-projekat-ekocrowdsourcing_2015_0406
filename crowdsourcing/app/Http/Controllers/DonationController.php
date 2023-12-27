b2590da6bce5607673ccb78baaa69ae94c0eb633
markoi95 <marko.ivanovic95@outlook.com>
2023-12-27 23:16:15 +0100

    Donation resource and controller (index and show) working

---
+++ crowdsourcing/app/Http/Controllers/DonationController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\DonationResource;
+use App\Models\Donation;
+use Illuminate\Http\Request;
+
+class DonationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $donation = Donation::all();
+
+        return $donation;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Donation $donation)
+    {
+        return new DonationResource($donation);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Donation $donation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Donation $donation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Donation $donation)
+    {
+        //
+    }
+}
