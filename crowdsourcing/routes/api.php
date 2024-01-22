29c205d458bac9b3cfe0ae636e635c82a29a88a1
markoi95 <marko.ivanovic95@outlook.com>
2024-01-22 15:06:24 +0100

    Authentication done with register function & route

--- crowdsourcing/routes/api.php
+++ crowdsourcing/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ProjectDonationController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserProjectController;
+use App\Http\Controllers\API\AuthController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,7 @@ Route::resource('projects.donations', ProjectDonationController::class);
 Route::delete('/delete-project/{id}', [ProjectController::class,'destroy']);
 Route::patch('/update-user/{id}', [UserController::class, 'update']);
 Route::post('/create-donation', [DonationController::class, 'store']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
