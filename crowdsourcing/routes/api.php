290fda09bb21517130d4b41476ad229bdcf81998
markoi95 <marko.ivanovic95@outlook.com>
2024-01-23 15:30:42 +0100

    kernel route middleware added; api routes sorted by groups: logged & admin users

--- crowdsourcing/routes/api.php
+++ crowdsourcing/routes/api.php
@@ -16,17 +16,28 @@ Route::get('/projects/{id}', [ProjectController::class,'show']); */
 /* Route::get('/users/{user_id}/projects', [UserProjectController::class,'index']); */
 
 Route::resource('projects', ProjectController::class);
-Route::resource('users', UserController::class);
-Route::resource('users.projects', UserProjectController::class);
 Route::resource('donations', DonationController::class);
 Route::resource('projects.donations', ProjectDonationController::class);
-Route::delete('/delete-project/{id}', [ProjectController::class,'destroy']);
-Route::patch('/update-user/{id}', [UserController::class, 'update']);
-Route::post('/create-donation', [DonationController::class, 'store']);
+
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
 
+ Route::group(['middleware' => ['auth:sanctum']], function () {
+        // samo ulogovani korisnici mogu da pristupe ovim rutama
+        Route::patch('/update-user/{id}', [UserController::class, 'update']);
+        Route::post('/create-donation', [DonationController::class, 'store']);
+        Route::resource('users.projects', UserProjectController::class);
+});
 
+Route::group(['middleware' => ['auth:sanctum', 'admin']], function () {       
+    // samo admini mogu da pristupe ovim rutama   
+        Route::delete('/delete-project/{id}', [ProjectController::class,'destroy']);
+        Route::resource('users', UserController::class);
+        Route::get('/admin-dashboard', function () {
+        return 'Welcome to the admin dashboard!';
+    });
 });
