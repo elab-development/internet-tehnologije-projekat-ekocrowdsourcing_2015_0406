0ba6f8801b27b8805e230bb0e0225606ab4ddce6
markoi95 <marko.ivanovic95@outlook.com>
2024-02-08 23:48:40 +0100

    "Already logged in" check

--- crowdsourcing/app/Http/Controllers/API/AuthController.php
+++ crowdsourcing/app/Http/Controllers/API/AuthController.php
@@ -38,11 +38,17 @@ class AuthController extends Controller
 
     public function login(Request $request)
 {
-    if (!Auth::attempt($request->only('email', 'password'))) {
-        return response()->json(['message' => 'Unauthorized'], 401);
+    if ($request->bearerToken()) {
+        return response()->json(['message' => 'Already logged in'], 200);
     }
 
+    if (!Auth::attempt($request->only('email', 'password'))) {
+        return response()->json(['message' => 'Wrong credentials'], 401);
+    }
+
+
     $user = User::where('email', $request['email'])->firstOrFail();
+
 
     // Authenticate the user in the session
     Auth::login($user);
