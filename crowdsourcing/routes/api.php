1dc803d23dd73e49c56759e7bd5a16983007aed6
markoi95 <marko.ivanovic95@outlook.com>
2024-05-27 14:43:39 +0200

    polish

--- crowdsourcing/routes/api.php
+++ crowdsourcing/routes/api.php
@@ -31,29 +31,24 @@ Route::group(['middleware' => ['auth:sanctum', 'mod']], function () {
 });
 
 Route::group(['middleware' => ['auth:sanctum','admin-or-mod']], function () {       
-    // admin i mod mogu da pristupe ovim rutama
-    Route::resource('users', UserController::class)->only(['show','index']); //radi za admin ili mod
-    Route::resource('projects', ProjectController::class); //except zbog errora, overlap pretpostavljam?  
-    //radi za admin i mod
-    Route::delete('/delete-project/{id}', [ProjectController::class,'destroy']); //radi za admin i mod
+    Route::resource('users', UserController::class)->only(['show','index']);
+    Route::resource('projects', ProjectController::class);
+    Route::delete('/delete-project/{id}', [ProjectController::class,'destroy']); 
 });
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    // samo ulogovani korisnici mogu da pristupe ovim rutama
     Route::patch('/update-user', [UserController::class, 'updateMe']);
     Route::get('/profile', [UserController::class, 'showCurrent']);
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/create-donation', [DonationController::class, 'store']); //radi za sva 3 ulogovana
-    Route::resource('users.projects', UserProjectController::class)->only(['show','index']); //radi za sva 3 ulogovana
-    Route::resource('projects', ProjectController::class)->only(['store']);  //radi za sva 3, samo store, update nije proveren
-
+    Route::post('/create-donation', [DonationController::class, 'store']); 
+    Route::resource('users.projects', UserProjectController::class)->only(['show','index']); 
+    Route::resource('projects', ProjectController::class)->only(['store']);
 });
 
 //svi mogu
 Route::resource('donations', DonationController::class)->only(['show','index']); //radi
 Route::resource('projects.donations', ProjectDonationController::class)->only([/* 'show', */'index']); //radi
 Route::resource('projects', ProjectController::class)->only(['show','index']); //radi
-
 Route::post('/register', [AuthController::class, 'register']); //radi
 Route::post('/login', [AuthController::class, 'login']); //radi
 
