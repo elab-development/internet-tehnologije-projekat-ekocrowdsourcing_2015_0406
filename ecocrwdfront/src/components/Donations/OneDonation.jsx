98deb75b9de84b1b157d9ec17e07f0a0ca99cd1d
markoi95 <marko.ivanovic95@outlook.com>
2024-06-29 22:47:22 +0200

    added buttons for admins

--- ecocrwdfront/src/components/Donations/OneDonation.jsx
+++ ecocrwdfront/src/components/Donations/OneDonation.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 
-const OneDonation = ({donation}) => {
+const OneDonation = ({donation, userRole, handleDelete, handleShowModal}) => {
+
   return (
     <tr>
     <td>{donation.email}</td>
     <td>{donation.description}</td>
     <td>{donation.project_id}</td>
     <td>{donation.amount}</td>
+    {userRole === 'admin' && (
+        <td>
+          <div className="d-inline">
+          <button
+            className="btn btn-primary btn-sm mr-2"
+              onClick={() => handleShowModal(donation.id)} 
+          >
+            Update
+          </button>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => handleDelete(donation.id)} 
+          >
+            Delete
+          </button>
+          </div>
+        </td>
+      )}
   </tr>
   )
 }
