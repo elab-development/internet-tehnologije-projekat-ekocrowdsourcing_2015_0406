d5f360c34ae8d1a1201acac42be2c367873448dd
markoi95 <marko.ivanovic95@outlook.com>
2024-07-11 19:57:40 +0200

    User ID displayed in response

--- crowdsourcing/app/Http/Resources/UserResource.php
+++ crowdsourcing/app/Http/Resources/UserResource.php
@@ -16,6 +16,7 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'=>$this->resource->id,
             'name'=>$this->resource->name,
             'email'=>$this->resource->email,
             'type'=>$this->resource->type, 
