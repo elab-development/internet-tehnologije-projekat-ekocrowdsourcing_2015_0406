aac17dd9c50a25c5ab729176b57992139e9c8d86
markoi95 <marko.ivanovic95@outlook.com>
2024-07-13 01:02:03 +0200

    add new project button; button color css

--- ecocrwdfront/src/components/Projects/Projects.jsx
+++ ecocrwdfront/src/components/Projects/Projects.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import ProjectCard from '../Reusable/ProjectCard';
 import ProjectModal from '../Reusable/ProjectModal';
+import ProjectPopup from '../Reusable/ProjectPopup';
+import { IoMdAddCircle } from "react-icons/io";
 
 const Projects = ({types, handleEdit, handleSave, handleCloseModal, handleShowModal,showModal,formData,setFormData, userRole, token, projects, currentPage, setCurrentPage, totalPages, handleDelete, handleOpenDonationCreateModal}) => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
 
+  const handleCardClick = async (projectId) => {
+    try {
+      const response = await axios.get(`/api/projects/${projectId}`);
+      setSelectedProject(response.data.Project);
+      setShowPopup(true);
+      console.log(response.data.Project);
+    } catch (error) {
+      console.error('Error fetching project data:', error);
+    }
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+    setSelectedProject(null);
+  };
   const handlePrevPage = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
@@ -19,19 +40,18 @@ const Projects = ({types, handleEdit, handleSave, handleCloseModal, handleShowMo
   
   return (
     <div className="container mt-3">
-
-
       <h2>Projects on our platform:</h2>
       {token === null? "Log in to create a project!" : 
-      <button type="button" className="btn btn-primary" onClick={handleShowModal}>
-      Add new project</button>}
+      <button type="button" className="btn btn-color text-white ms-3" onClick={handleShowModal}>
+      <IoMdAddCircle /> Add new project</button>}
 
         <ProjectModal show={showModal} handleClose={handleCloseModal} handleSave={handleSave} token={token} types={types} formData={formData} setFormData={setFormData}/>
       <div className="row">
 
         {projects === null ? "No projects" : projects.map((proj)=>(
             <div className="col-md-4 mb-4" key={proj.id}>
-            <ProjectCard project={proj} handleDelete={handleDelete} userRole={userRole} handleEdit={handleEdit} handleOpenDonationCreateModal={handleOpenDonationCreateModal}/>
+            <ProjectCard project={proj} handleDelete={handleDelete} userRole={userRole} handleEdit={handleEdit} handleOpenDonationCreateModal={handleOpenDonationCreateModal} 
+            handleCardClick={handleCardClick}/>
             </div>
         ))}
 
@@ -40,7 +60,7 @@ const Projects = ({types, handleEdit, handleSave, handleCloseModal, handleShowMo
       <div className="row justify-content-between">
         <div className="col-1">
           <button 
-            className="btn btn-primary mb-3" 
+            className="btn btn-color mb-3 text-white" 
             onClick={handlePrevPage} 
             disabled={currentPage === 1}>
             &lt;
@@ -48,13 +68,16 @@ const Projects = ({types, handleEdit, handleSave, handleCloseModal, handleShowMo
         </div>
         <div className="col-1 text-end">
           <button 
-            className="btn btn-primary mb-3" 
+            className="btn btn-color mb-3 text-white" 
             onClick={handleNextPage} 
             disabled={currentPage === totalPages}>
             &gt;
           </button>
         </div>
       </div>
+      {selectedProject && showPopup && (
+        <ProjectPopup project={selectedProject} handleClose={handleClosePopup}/>
+      )}
     </div>
   )
 }
