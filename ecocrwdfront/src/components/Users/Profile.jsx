a02e5e9e26b3eaed210b6c32c4e7f08a65264151
markoi95 <marko.ivanovic95@outlook.com>
2024-07-13 01:16:02 +0200

    func const; update user btn link

--- ecocrwdfront/src/components/Users/Profile.jsx
+++ ecocrwdfront/src/components/Users/Profile.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { FaUserEdit } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Profile = ({token}) => {
     const[userData, setUserData]=useState({
@@ -9,29 +11,26 @@ const Profile = ({token}) => {
         role:"",
     });
 
-    const fetchProfile = (token) => {
+    const fetchProfile = async (token) => {
         if(token===null){
           return;
         }
-        axios.get('api/profile', {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((response) => {
+        try {
+          const response = await axios.get('api/profile', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
           setUserData(response.data.User);
           console.log(response.data.User);
-        })
-        .catch((error) => {
+        } catch (error) {
           console.error(error);
-        });
+        }
     }
 
     useEffect(() => {
         fetchProfile(token)
       }, [token]);
-
-
 
   return (
     <div className="container mt-5 d-flex justify-content-center">
@@ -41,6 +40,9 @@ const Profile = ({token}) => {
           <p className="card-text"><strong>Name:</strong> {userData.name} </p>
           <p className="card-text"><strong>Email:</strong> {userData.email}</p>
           <p className="card-text"><strong>Role:</strong> {userData.type}</p>
+          <Link to="/update-user" className="btn btn-color text-white ms-3 me-3 mb-2">
+                <FaUserEdit /> Update
+          </Link>
         </div>
       </div>
     </div>
